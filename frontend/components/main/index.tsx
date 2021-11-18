6d2c9df5ece79e11325802aebe5e75e743e067b8
Beatriz Morais <beatriz.morais@ufba.br>
2021-11-18 12:37:53 -0300

    add styled components

--- frontend/components/main/index.tsx
+++ frontend/components/main/index.tsx
@@ -3,6 +3,37 @@ import { StyledMain } from './styles'
 export default function Main() {
     return(
     <StyledMain>
+        <h1>Confirmar Pedido</h1>
+        <div className='alignInputs'>
+            <label>Nome do constribuidor </label>
+            <input type='text' placeholder=''/>
+            <label>Reservado por </label>
+            <input type='' placeholder=''/>
+        </div>
+
+        <div className='alignInputs'>
+            <label>Origem</label>
+            <input type='text' placeholder='' />
+            <label>Destino</label>
+            <input type='text' placeholder='' />
+        </div>
+
+        <div className='alignInputs'>
+            <label>Componente</label>
+            <input type='text' />
+            <label>Nome do componente </label>
+            <input type='text' />
+        </div>
+
+        <div className='alignInputs'>
+            <label>Quantidade</label>
+            <input type='text' />
+            <div className='alignButtons'>
+                <button id='cancel'>Cancelar</button>
+                <button id='send'>Enviar</button>
+            </div>
+        </div>
+        
     </StyledMain>
     )
 }
--- frontend/components/main/styles.js
+++ frontend/components/main/styles.js
@@ -1,6 +1,38 @@
 import styled from "styled-components";
 
 export const StyledMain = styled.main`
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,600;0,700;1,100;1,200&display=swap');
+    
+    font-family: 'Poppins';
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    padding: 5.5em;
+    background-color: #10142D;
+    color: #96969C;
+
+    .alignInputs {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between; 
+    }
+
+    input {
+        background-color: transparent;
+        border-radius: 30px;
+        border: 1px solid white;
+        margin: 0.5%;
+        min-height: 50px;
+        min-weight: 100%;
+    }
+
+    #cancel {
+        font-family: 'Poppins';
+        font-size: 10px;
+        font-weight: 600;
+        padding: 5px;
+        background-color: #96969C;
+        color: white;
+        border: none;
+    }
+
 `;
