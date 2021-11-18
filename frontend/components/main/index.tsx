d0c377497530fd7d7ced6f8f2ae1e262ee92ffca
Beatriz Morais <beatriz.morais@ufba.br>
2021-11-18 19:31:49 -0300

    Finishing main

--- frontend/components/main/index.tsx
+++ frontend/components/main/index.tsx
@@ -5,30 +5,42 @@ export default function Main() {
     <StyledMain>
         <h1>Confirmar Pedido</h1>
         <div className='alignInputs'>
-            <label>Nome do constribuidor </label>
-            <input type='text' placeholder=''/>
-            <label>Reservado por </label>
-            <input type='' placeholder=''/>
+            <label className='label-input'>
+                <span> Nome do constribuidor </span>
+            <input type='text' placeholder=''/></label>
+
+            <label className='label-input'>
+                <span>Reservado por</span>
+            <input type='' placeholder=''/></label>
         </div>
 
         <div className='alignInputs'>
-            <label>Origem</label>
-            <input type='text' placeholder='' />
-            <label>Destino</label>
-            <input type='text' placeholder='' />
+            <label className='label-input'> <span>Origem</span>
+            <input type='text' placeholder='' /></label>
+
+            <label className='label-input' >
+                <span>Destino</span>
+            <input type='text' placeholder='' /></label>
         </div>
 
         <div className='alignInputs'>
-            <label>Componente</label>
-            <input type='text' />
-            <label>Nome do componente </label>
-            <input type='text' />
+            <label className='label-input'>
+                <span>Componente</span>
+            <input type='text' /></label>
+
+            <label className='label-input'>
+                <span>Nome do componente </span>
+            <input type='text' /></label>
+
         </div>
 
         <div className='alignInputs'>
-            <label>Quantidade</label>
-            <input type='text' />
+            <label className='label-inputEnd'>
+                <span>Quantidade</span>
+            <input type='text' /></label>
+
             <div className='alignButtons'>
+
                 <button id='cancel'>Cancelar</button>
                 <button id='send'>Enviar</button>
             </div>
--- frontend/components/main/styles.js
+++ frontend/components/main/styles.js
@@ -4,35 +4,59 @@ export const StyledMain = styled.main`
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,600;0,700;1,100;1,200&display=swap');
     
     font-family: 'Poppins';
-    display: flex;
-    flex-direction: column;
-    padding: 5.5em;
     background-color: #10142D;
     color: #96969C;
+    padding: 4% 10% 5% 10%;
+
+    h1 {
+        font-weight: 600;
+    }
 
     .alignInputs {
         display: flex;
         flex-direction: row;
-        justify-content: space-between; 
+    }
+
+    .alignButtons {
+        display: flex;
+        flex-content: row;
     }
 
     input {
-        background-color: transparent;
-        border-radius: 30px;
+        background-color: transparent; 
+        border-radius: 60px;
         border: 1px solid white;
-        margin: 0.5%;
-        min-height: 50px;
-        min-weight: 100%;
+        min-height: 40px;
+    }
+
+    .label-input {
+        display: flex;
+        flex-direction: column;
+        width: 90%;
+        margin: 1%;
+    }
+
+    .label-inputEnd {
+        display: flex;
+        flex-direction: column;
+        width: 50%;
+        margin: 1%;
+    }
+
+    button {
+        font-family: 'Poppins';
+        color: white;
+        font-weight: 500;
+        border: none;
+        cursor: pointer;
     }
 
     #cancel {
-        font-family: 'Poppins';
-        font-size: 10px;
-        font-weight: 600;
-        padding: 5px;
         background-color: #96969C;
-        color: white;
-        border: none;
+        
     }
 
+    #send {
+        background-color: #006466;        
+    }
 `;
