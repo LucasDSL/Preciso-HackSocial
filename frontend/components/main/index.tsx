a0f2f053207f514062e422726a72c89da3a41a99
Beatriz Morais <beatriz.morais@ufba.br>
2021-11-19 17:10:32 -0300

    few adjustments

--- frontend/components/main/index.tsx
+++ frontend/components/main/index.tsx
@@ -2,6 +2,7 @@ import { StyledMain } from './styles'
 
 export default function Main() {
     return(
+        
     <StyledMain>
         <h1>Confirmar Pedido</h1>
         <div className='alignInputs'>
